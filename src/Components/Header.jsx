4131c10d718a34e7a392246083a713d8d1dbc9c7
Jannick Nielsen <jannicktrollenielsen@hotmail.com>
2023-02-02 09:44:00 +0100

    somthing

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,21 @@
 import { Link } from "react-router-dom"
 import styled from "styled-components"
+import mailIcon from "../Assets/images/mail-icon.png"
+import phoneIcon from "../Assets/images/phone-icon.png"
+import CartIcon from "../Assets/images/cart-icon.png"
 
 const StyledHeader = styled.header`
     nav {
         ul {
+            display: flex;
+            a {
+                color: #ffffff;
+            }
+        }
+        div {
+            div {
+                color: #ffffff;
+            }
         }
     }
 `
@@ -12,6 +24,7 @@ export default function Header() {
     return (
         <StyledHeader>
             <nav>
+                <img src="" alt="" />
                 <ul>
                     <Link to="/">
                         <li>Forside</li>
@@ -23,6 +36,20 @@ export default function Header() {
                         <li>Login</li>
                     </Link>
                 </ul>
+                <div>
+                    <div>
+                        <img src={mailIcon} alt="icon" />
+                        <img src={phoneIcon} alt="icon" />
+                        <img src={CartIcon} alt="icon" />
+                    </div>
+                    <div>
+                        <input type="text" placeholder="indtast søgeord" />
+                        <button>
+                            {" "}
+                            <i class="fa-solid fa-arrow-right"></i>{" "}
+                        </button>
+                    </div>
+                </div>
             </nav>
         </StyledHeader>
     )
